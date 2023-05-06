4fc0c13f7b1aceff5b90a4ed530eda61945736d2
Jordi Prats <jordi.prats@gmail.com>
2023-05-06 16:51:05 +0200

    initial

--- main.go
+++ main.go
@@ -1,38 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/autoscaling"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
 var (
 	autoscalingClient *autoscaling.AutoScaling
 	ec2Client         *ec2.EC2
 
-	setRegion           = os.Getenv("REGION")
-	caNamespace         = os.Getenv("CA_NAMESPACE")
-	caPriorityExpander  = "cluster-autoscaler-priority-expander"
-	asgContains         = os.Getenv("ASG_CONTAINS")
-	ltContains          = os.Getenv("LT_CONTAINS")
-	sleepMinutesEnv     = os.Getenv("SLEEP_MINUTES")
-	sleepMinutes        int
-	loopSleep           time.Duration
-	catchAllEnv         = os.Getenv("CATCH_ALL")
-	catchAll            bool
-	debugEnv            = os.Getenv("DEBUG")
-	debug               bool
-	skipCMCreationEnv   = os.Getenv("SKIP_CM_CREATION")
-	skipCMCreation      bool
+	setRegion          = os.Getenv("REGION")
+	caNamespace        = os.Getenv("CA_NAMESPACE")
+	caPriorityExpander = "cluster-autoscaler-priority-expander"
+	asgContains        = os.Getenv("ASG_CONTAINS")
+	ltContains         = os.Getenv("LT_CONTAINS")
+	sleepMinutesEnv    = os.Getenv("SLEEP_MINUTES")
+	sleepMinutes       int
+	loopSleep          time.Duration
+	catchAllEnv        = os.Getenv("CATCH_ALL")
+	catchAll           bool
+	debugEnv           = os.Getenv("DEBUG")
+	debug              bool
+	skipCMCreationEnv  = os.Getenv("SKIP_CM_CREATION")
+	skipCMCreation     bool
 )
 
 func init() {
@@ -99,116 +103,114 @@ func mainLoop() {
 				subnet, _ := ec2Client.DescribeSubnets(&ec2.DescribeSubnetsInput{
 					SubnetIds: []*string{&subnetID},
 				})
-				freeIPs += int(*subnet.Subnets[	0].AvailableIpAddressCount)
-		}
+				freeIPs += int(*subnet.Subnets[0].AvailableIpAddressCount)
+			}
 
-		if _, ok := caPriorities[freeIPs]; !ok {
-			caPriorities[freeIPs] = []string{*asg.AutoScalingGroupName}
+			if _, ok := caPriorities[freeIPs]; !ok {
+				caPriorities[freeIPs] = []string{*asg.AutoScalingGroupName}
+			} else {
+				caPriorities[freeIPs] = append(caPriorities[freeIPs], *asg.AutoScalingGroupName)
+			}
+
+			if debug {
+				fmt.Printf("%s/%s has %d free IPs\n", *asg.AutoScalingGroupName, ltName, freeIPs)
+			}
+		}
+	}
+
+	// Initialize Kubernetes client
+	config, err := clientcmd.BuildConfigFromFlags("", "")
+	if err != nil {
+		fmt.Printf("Unable to load kube config: %v\n", err)
+		return
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("Unable to create Kubernetes client: %v\n", err)
+		return
+	}
+
+	// Check if configmap exists
+	configMapExists := false
+	_, err = clientset.CoreV1().ConfigMaps(caNamespace).Get(context.Background(), caPriorityExpander, metav1.GetOptions{})
+	if err == nil {
+		configMapExists = true
+	}
+
+	// Save config
+	data := make(map[string]string)
+	priorities := ""
+	keys := make([]int, 0, len(caPriorities))
+	for k := range caPriorities {
+		keys = append(keys, k)
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(keys)))
+
+	for _, key := range keys {
+		priorities += fmt.Sprintf("%d:\n", key)
+		for _, asg := range caPriorities[key] {
+			priorities += fmt.Sprintf("  - %s\n", asg)
+		}
+	}
+
+	if catchAll {
+		priorities += "1:\n"
+		priorities += "  - .*\n"
+	}
+
+	data["priorities"] = priorities
+
+	if debug {
+		fmt.Println(data["priorities"])
+	}
+
+	if !configMapExists {
+		if skipCMCreation {
+			fmt.Printf("Skipping creation of configmap: %s/%s\n", caNamespace, caPriorityExpander)
 		} else {
-			caPriorities[freeIPs] = append(caPriorities[freeIPs], *asg.AutoScalingGroupName)
+			_, err := clientset.CoreV1().ConfigMaps(caNamespace).Create(context.Background(), &v1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: caPriorityExpander,
+				},
+				Data: data,
+			}, metav1.CreateOptions{})
+			if err != nil {
+				fmt.Printf("Error creating configmap: %v\n", err)
+			} else {
+				fmt.Printf("Created configmap: %s/%s\n", caNamespace, caPriorityExpander)
+			}
 		}
-
-		if debug {
-			fmt.Printf("%s/%s has %d free IPs\n", *asg.AutoScalingGroupName, ltName, freeIPs)
-		}
-	}
-}
-
-// Initialize Kubernetes client
-config, err := clientcmd.BuildConfigFromFlags("", "")
-if err != nil {
-	fmt.Printf("Unable to load kube config: %v\n", err)
-	return
-}
-clientset, err := kubernetes.NewForConfig(config)
-if err != nil {
-	fmt.Printf("Unable to create Kubernetes client: %v\n", err)
-	return
-}
-
-// Check if configmap exists
-configMapExists := false
-_, err = clientset.CoreV1().ConfigMaps(caNamespace).Get(caPriorityExpander, metav1.GetOptions{})
-if err == nil {
-	configMapExists = true
-}
-
-// Save config
-data := make(map[string]string)
-priorities := ""
-keys := make([]int, 0, len(caPriorities))
-for k := range caPriorities {
-	keys = append(keys, k)
-}
-sort.Sort(sort.Reverse(sort.IntSlice(keys)))
-
-for _, key := range keys {
-	priorities += fmt.Sprintf("%d:\n", key)
-	for _, asg := range caPriorities[key] {
-		priorities += fmt.Sprintf("  - %s\n", asg)
-	}
-}
-
-if catchAll {
-	priorities += "1:\n"
-	priorities += "  - .*\n"
-}
-
-data["priorities"] = priorities
-
-if debug {
-	fmt.Println(data["priorities"])
-}
-
-if !configMapExists {
-	if skipCMCreation {
-		fmt.Printf("Skipping creation of configmap: %s/%s\n", caNamespace, caPriorityExpander)
 	} else {
-		_, err := clientset.CoreV1().ConfigMaps(caNamespace).Create(&corev1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name: caPriorityExpander,
-			},
-			Data: data,
-		})
+		cm, err := clientset.CoreV1().ConfigMaps(caNamespace).Get(context.Background(), caPriorityExpander, metav1.GetOptions{})
 		if err != nil {
-			fmt.Printf("Error creating configmap: %v\n", err)
-		} else {
-			fmt.Printf("Created configmap: %s/%s\n", caNamespace, caPriorityExpander)
+			fmt.Printf("Error retrieving configmap: %v\n", err)
+			return
 		}
+		cm.Data = data
+		_, err = clientset.CoreV1().ConfigMaps(caNamespace).Update(context.Background(), cm, metav1.UpdateOptions{})
+		if err != nil {
+			fmt.Printf("Error updating configmap: %v\n", err)
+			return
+		}
+		fmt.Printf("Updated configmap: %s/%s\n", caNamespace, caPriorityExpander)
 	}
-} else {
-	cm, err := clientset.CoreV1().ConfigMaps(caNamespace).Get(caPriorityExpander, metav1.GetOptions{})
-	if err != nil {
-		fmt.Printf("Error retrieving configmap: %v\n", err)
-		return
-	}
-	cm.Data = data
-	_, err = clientset.CoreV1().ConfigMaps(caNamespace).Update(cm)
-	if err != nil {
-		fmt.Printf("Error updating configmap: %v\n", err)
-		return
-	}
-	fmt.Printf("Updated configmap: %s/%s\n", caNamespace, caPriorityExpander)
-}
 
 }
 
 func awsSearchEC2ASGByName(name string) []*autoscaling.Group {
-var records []*autoscaling.Group
+	var records []*autoscaling.Group
 
-go
-
-err := autoscalingClient.DescribeAutoScalingGroupsPages(&autoscaling.DescribeAutoScalingGroupsInput{},
-	func(page *autoscaling.DescribeAutoScalingGroupsOutput, lastPage bool) bool {
-		for _,group := range page.AutoScalingGroups {
-if strings.Contains(*group.AutoScalingGroupName, name) {
-records = append(records, group)
-}
-}
-return !lastPage
-})
-if err != nil {
-fmt.Printf("Error searching EC2 ASGs by name: %v\n", err)
-}
-return records
+	err := autoscalingClient.DescribeAutoScalingGroupsPages(&autoscaling.DescribeAutoScalingGroupsInput{},
+		func(page *autoscaling.DescribeAutoScalingGroupsOutput, lastPage bool) bool {
+			for _, group := range page.AutoScalingGroups {
+				if strings.Contains(*group.AutoScalingGroupName, name) {
+					records = append(records, group)
+				}
+			}
+			return !lastPage
+		})
+	if err != nil {
+		fmt.Printf("Error searching EC2 ASGs by name: %v\n", err)
+	}
+	return records
 }
